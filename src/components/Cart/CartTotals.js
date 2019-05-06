22778c28b66c5790ac772a24f5d345b51cb89766
siddhantsingh1997 <siddhant.singh5120@yahoo.in>
2019-05-07 05:10:31 +0530

    Paypal button added

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-
+import Paypalbutton from "./PaypalButton";
 import { Link } from "react-router-dom";
-export default class CartTotals extends Component {
-  render() {
+export default function CartTotals({ value, history }) {
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  {
+    /*render() {
     const {
       cartSubTotal,
       cartTax,
@@ -11,41 +13,44 @@ export default class CartTotals extends Component {
       clearCart
     } = this.props.value;
     const { history } = this.props;
-    const emptyCart = cart.length === 0 ? true : false;
-    return (
-      <React.Fragment>
-        {!emptyCart && (
-          <div className="container">
-            <div className="row">
-              <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-                <Link to="/">
-                  <button
-                    className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                    type="button"
-                    onClick={() => {
-                      clearCart();
-                    }}
-                  >
-                    clear cart
-                  </button>
-                </Link>
-                <h5>
-                  <span className="text-title"> subtotal :</span>{" "}
-                  <strong>$ {cartSubTotal} </strong>
-                </h5>
-                <h5>
-                  <span className="text-title"> tax :</span>{" "}
-                  <strong>$ {cartTax} </strong>
-                </h5>
-                <h5>
-                  <span className="text-title"> total :</span>{" "}
-                  <strong>$ {cartTotal} </strong>
-                </h5>
-              </div>
-            </div>
-          </div>
-        )}
-      </React.Fragment>
-    );
+  const emptyCart = cart.length === 0 ? true : false;*/
   }
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button
+                className="btn btn-outline-danger text-uppercase mb-3 px-5"
+                type="button"
+                onClick={() => {
+                  clearCart();
+                }}
+              >
+                clear cart
+              </button>
+            </Link>
+            <h5>
+              <span className="text-title"> subtotal :</span>{" "}
+              <strong>$ {cartSubTotal} </strong>
+            </h5>
+            <h5>
+              <span className="text-title"> tax :</span>{" "}
+              <strong>$ {cartTax} </strong>
+            </h5>
+            <h5>
+              <span className="text-title"> total :</span>{" "}
+              <strong>$ {cartTotal} </strong>
+            </h5>
+            <Paypalbutton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
